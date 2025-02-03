57eeb3206f844c29d7082b41ed5978478b185541
sajid1108 <cahsekolahbelajar@gmail.com>
2025-02-03 07:22:24 +0700

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import profilePicture from "../assets/profile-picture.png";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import "../styles/Header.css";
 function Header() {
   return (
     <header>
-      <div className="header-jombotron">
+      <div className="header-jumbotron">
         <img src={profilePicture} />
         <h3>Sajid Abdul Fatah</h3>
         <p>Student - Biginner Front End Developer</p>
+        <div className="socialMedia">
+          <a href="">
+            <FaInstagram />
+          </a>
+          <a href="https://github.com/sajid1108">
+            <FaGithub />
+          </a>
+        </div>
       </div>
     </header>
   );
